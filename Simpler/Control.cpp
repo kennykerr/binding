f087b2b17d7085e43fb88255ce26aee6b943bf06
Kenny Kerr <kekerr@microsoft.com>
2019-04-10 11:27:36 -0700

    simpler

--- Simpler/Control.cpp
+++ Simpler/Control.cpp
@@ -12,6 +12,11 @@ struct SampleControl : xaml_user_control<SampleControl>
     hstring m_text;
     IObservableVector<boxed_value> m_list{ observable_vector<boxed_value>() };
 
+    static hstring type_name()
+    {
+        return L"Sample.Control";
+    }
+
     xaml_member bind(hstring const& name)
     {
         if (name == L"Counter")
@@ -49,10 +54,5 @@ struct SampleControl : xaml_user_control<SampleControl>
 
             m_list.Append(m_counter);
         }
-    }
-
-    static hstring type_name()
-    {
-        return L"Sample.Control";
     }
 };
