7f46474647e535073379551535a8e42f91d7db7e
Kenny Kerr <kekerr@microsoft.com>
2019-06-25 11:33:26 -0700

    partition

--- Simpler/xaml.h
+++ Simpler/xaml.h
@@ -1,25 +1,10 @@
-// This header becomes "winrt/xaml.h" and shipped with C++/WinRT
-
 #pragma once
 
-namespace winrt::impl
+// TODO: This goes in windows.foundation.h
+namespace winrt
 {
     template <typename T>
-    struct bind_member
-    {
-        static Windows::Foundation::IInspectable get(T const& object, hstring const&)
-        {
-            return box_value(object);
-        }
-
-        static void set(T& object, hstring const&, Windows::Foundation::IInspectable const& value)
-        {
-            object = unbox_value<T>(value);
-        }
-    };
-
-    template <typename T>
-    class has_bind
+    class has_bind_member
     {
         template <typename U, typename = decltype(std::declval<U>().bind(L""))> static constexpr bool get_value(int) { return true; }
         template <typename> static constexpr bool get_value(...) { return false; }
@@ -28,163 +13,153 @@ namespace winrt::impl
 
         static constexpr bool value = get_value<T>(0);
     };
-}
 
-namespace winrt
-{
-    // Type returned by winrt::bind function to provide type inference
-    struct xaml_binding
+    template <typename T>
+    class has_bind_free
     {
-        xaml_binding() noexcept {}
+        template <typename U, typename = decltype(winrt::bind(std::declval<U>(), L""))> static constexpr bool get_value(int) { return true; }
+        template <typename> static constexpr bool get_value(...) { return false; }
 
-        template <typename T>
-        xaml_binding(T& value) : m_accessor{ new accessor<T>(value), take_ownership_from_abi }
+    public:
+
+        static constexpr bool value = get_value<T>(0);
+    };
+
+    template <typename T>
+    inline constexpr bool can_bind_v = has_bind_member<T>::value || has_bind_free<T>::value;
+
+    template <typename T>
+    auto bind_member(T&& object, hstring const& name)
+    {
+        if constexpr (has_bind_free<T>::value)
+        {
+            return winrt::bind(object, name);
+        }
+        else
+        {
+            return object.bind(name);
+        }
+    }
+
+    struct binding
+    {
+        binding() noexcept
         {
         }
 
-        template <typename T>
-        xaml_binding(T& value, hstring const& name) : m_accessor{ new accessor<T>(value, name), take_ownership_from_abi }
+        // TODO: add overloads that take leading strong/weak reference
+        template <typename T, typename = std::enable_if_t<!std::is_base_of_v<Windows::Foundation::IUnknown, T>>>
+        binding(T& reference) : binding
         {
-        }
-
-        Windows::Foundation::IInspectable get() const
-        {
-            if (m_accessor)
+            [&]
             {
-                return m_accessor->get();
+                return reference;
+            },
+            [&](T const& value)
+            {
+                reference = value;
+            }
+        }
+        {
+        }
+
+            template <typename T, typename = std::enable_if_t<std::is_base_of_v<Windows::Foundation::IUnknown, T>>>
+            binding(T const& object) : binding
+            {
+                [object]
+                {
+                    return object;
+                },
+                [](T const&) {}
+            }
+            {
             }
 
-            return {};
-        }
+                template <typename Get, typename Set>
+                binding(Get&& get_self, Set&& set_self) :
+                    m_accessor
+                {
+                    std::make_unique<accessor<Get, Set>>(
+                        std::forward<Get>(get_self),
+                        std::forward<Set>(set_self))
+                }
+                {
+                }
 
-        void set(Windows::Foundation::IInspectable const& value) const
-        {
-            if (m_accessor)
-            {
-                m_accessor->set(value);
-            }
-        }
+                Windows::Foundation::IInspectable get() const
+                {
+                    if (m_accessor)
+                    {
+                        return m_accessor->get();
+                    }
 
-        Windows::UI::Xaml::Interop::TypeName type() const
-        {
-            if (m_accessor)
-            {
-                return m_accessor->type();
-            }
+                    return nullptr;
+                }
 
-            return {};
-        }
+                void set(Windows::Foundation::IInspectable const& value) const
+                {
+                    if (m_accessor)
+                    {
+                        m_accessor->set(value);
+                    }
+                }
 
-        bool is_compound() const noexcept
-        {
-            return m_accessor && m_accessor->is_compound();
-        }
+                binding bind(hstring const& name) const
+                {
+                    if (m_accessor)
+                    {
+                        return m_accessor->bind(name);
+                    }
 
-        explicit operator bool() const noexcept
-        {
-            return static_cast<bool>(m_accessor);
-        }
+                    return {};
+                }
 
     private:
 
-        struct accessor_abi : impl::unknown_abi
+        struct accessor_abi
         {
             virtual Windows::Foundation::IInspectable get() = 0;
-            virtual void set(Windows::Foundation::IInspectable const&) = 0;
-            virtual Windows::UI::Xaml::Interop::TypeName type() const = 0;
-            virtual bool is_compound() const noexcept = 0;
+            virtual void set(Windows::Foundation::IInspectable const& value) = 0;
+            virtual binding bind(hstring const& name) = 0;
         };
 
-        template <typename T>
-        struct accessor final : accessor_abi
+        template <typename Get, typename Set>
+        struct accessor final : accessor_abi, Get, Set
         {
-            accessor(T& value) : m_value(value)
-            {
-            }
+            using type = decltype(std::declval<Get>()());
 
-            accessor(T& value, hstring const& name) : m_value(value), m_name(name)
+            accessor(Get&& get_self, Set&& set_self) :
+                Get(std::forward<Get>(get_self)),
+                Set(std::forward<Set>(set_self))
             {
             }
 
             Windows::Foundation::IInspectable get() final
             {
-                if constexpr (impl::has_category_v<T>)
+                return box_value((*this)());
+            }
+
+            void set(Windows::Foundation::IInspectable const& value) final
+            {
+                (*this)(unbox_value<type>(value));
+            }
+
+            binding bind([[maybe_unused]] hstring const& name) final
+            {
+                if constexpr (can_bind_v<type>)
                 {
-                    return impl::bind_member<T>::get(m_value, m_name);
-                }
-                else
-                {
-                    return {};
-                }
-            }
-
-            void set([[maybe_unused]] Windows::Foundation::IInspectable const& value) final
-            {
-                if constexpr (impl::has_category_v<T>)
-                {
-                    impl::bind_member<T>::set(m_value, m_name, value);
-                }
-            }
-
-            Windows::UI::Xaml::Interop::TypeName type() const final
-            {
-                return xaml_typename<T>();
-            }
-
-            bool is_compound() const noexcept final
-            {
-                return std::is_compound_v<T>;
-            }
-
-            int32_t __stdcall QueryInterface(guid const&, void** result) noexcept final
-            {
-                *result = nullptr;
-                return impl::error_no_interface;
-            }
-
-            uint32_t __stdcall AddRef() noexcept final
-            {
-                return 1 + m_references.fetch_add(1, std::memory_order_relaxed);
-            }
-
-            uint32_t __stdcall Release() noexcept final
-            {
-                uint32_t const target = m_references.fetch_sub(1, std::memory_order_release) - 1;
-
-                if (target == 0)
-                {
-                    std::atomic_thread_fence(std::memory_order_acquire);
-                    delete this;
+                    return bind_member((*this)(), name);
                 }
 
-                return target;
+                return {};
             }
-
-        private:
-
-            std::atomic<uint32_t> m_references{ 1 };
-            T& m_value;
-            hstring const m_name;
         };
 
-        com_ptr<accessor_abi> m_accessor;
+        std::unique_ptr<accessor_abi> m_accessor;
     };
-
-    // In winrt namespace so that developers can overload bind
-    template <typename T>
-    auto bind(T&& object, hstring const& name)
-    {
-        if constexpr (impl::has_bind<T>::value)
-        {
-            return object.bind(name);
-        }
-        else
-        {
-            return xaml_binding(object, name);
-        }
-    }
 }
 
+// TODO: this goes into windows.ui.xaml.h and forward declars the .Data/.Markup stuff?
 namespace winrt::impl
 {
     template <typename T>
@@ -193,12 +168,9 @@ namespace winrt::impl
     template <typename T>
     struct bind_property : implements<bind_property<T>, Windows::UI::Xaml::Data::ICustomProperty>
     {
-        bind_property(com_ptr<bind_object<T>>&& object, T& property, hstring const& name, winrt::delegate<T const&> const& update) :
+        bind_property(com_ptr<bind_object<T>>&& object, binding&& binding) :
             m_object(std::move(object)),
-            m_property(property),
-            m_name(name),
-            m_binding{ bind(m_property, m_name) },
-            m_update(update)
+            m_binding(std::move(binding))
         {
         }
 
@@ -210,12 +182,6 @@ namespace winrt::impl
         void SetValue(Windows::Foundation::IInspectable const&, Windows::Foundation::IInspectable const& value)
         {
             m_binding.set(value);
-
-            if (m_update)
-            {
-                m_update(m_property);
-            }
-
             m_object->property_changed(m_name);
         }
 
@@ -231,7 +197,7 @@ namespace winrt::impl
 
         Windows::UI::Xaml::Interop::TypeName Type() const noexcept
         {
-            return m_binding.type();
+            return xaml_typename<T>();
         }
 
         hstring Name() const noexcept
@@ -250,18 +216,14 @@ namespace winrt::impl
 
     private:
         com_ptr<bind_object<T>> m_object;
-        T m_property;
-        hstring m_name;
-        xaml_binding m_binding;
-        winrt::delegate<T const&> m_update;
+        binding m_binding;
     };
 
     template <typename T>
     struct bind_object : implements<bind_object<T>, Windows::UI::Xaml::Data::ICustomPropertyProvider, Windows::UI::Xaml::Data::INotifyPropertyChanged>
     {
-        bind_object(T const& object, winrt::delegate<T const&> const& update = nullptr) :
-            m_object(object),
-            m_update(update)
+        bind_object(T const& object) :
+            m_object(object)
         {
         }
 
@@ -282,7 +244,7 @@ namespace winrt::impl
 
         Windows::UI::Xaml::Data::ICustomProperty GetCustomProperty(hstring const& member)
         {
-            return make<bind_property<T>>(get_strong(), m_object, member, m_update);
+            return make<bind_property<T>>(get_strong(), bind_member(m_object, member));
         }
 
         Windows::UI::Xaml::Data::ICustomProperty GetIndexedProperty(hstring const&, Windows::UI::Xaml::Interop::TypeName const&) const noexcept
@@ -303,7 +265,6 @@ namespace winrt::impl
     private:
         T m_object;
         event<Windows::UI::Xaml::Data::PropertyChangedEventHandler> m_changed;
-        winrt::delegate<T const&> m_update;
     };
 
     struct xaml_registry
@@ -333,7 +294,7 @@ namespace winrt::impl
 
         inline static xaml_registry& registry() noexcept
         {
-            // TODO: Try to avoid magic static (hopefully std::map has constexpr zero-init constructor)
+            // TODO: Try to avoid magic static
             static xaml_registry s_registry;
             return s_registry;
         }
@@ -439,23 +400,17 @@ namespace winrt
             xaml_member(com_ptr<D>&& object, hstring const& name) :
                 m_object(std::move(object)),
                 m_name(name),
-                m_binding{ bind(*m_object, m_name) }
+                m_binding{ bind_member(*m_object, m_name) }
             {
             }
 
             Windows::Foundation::IInspectable GetValue(Windows::Foundation::IInspectable const&)
             {
-                if (!m_binding.is_compound() || m_value == nullptr)
-                {
-                    m_value = m_binding.get();
-                }
-
-                return m_value;
+                return m_binding.get();
             }
 
             void SetValue(Windows::Foundation::IInspectable const&, Windows::Foundation::IInspectable const& value)
             {
-                m_value = nullptr;
                 m_binding.set(value);
                 m_object->property_changed(m_name);
             }
@@ -494,8 +449,7 @@ namespace winrt
 
             com_ptr<D> m_object;
             hstring const m_name;
-            xaml_binding m_binding;
-            Windows::Foundation::IInspectable m_value;
+            binding m_binding;
         };
 
         struct xaml_type_instance : implements<xaml_type_instance, Windows::UI::Xaml::Markup::IXamlType>
@@ -571,8 +525,6 @@ namespace winrt
         event<Windows::UI::Xaml::Data::PropertyChangedEventHandler> m_changed;
     };
 
-    // -----------------
-
     template <typename D, typename... I>
     struct registered_implements : implements<D, I...>, xaml_registration<D, true>
     {
@@ -645,8 +597,6 @@ namespace winrt
             void RunInitializer() const noexcept { }
         };
     };
-
-    // -----------------
 
     template <typename D, typename... I>
     using xaml_page = xaml_type<D, false, Windows::UI::Xaml::Controls::PageT, I...>;
